e349c55803dc73382a27e54a4a17db62b78c2096
Kai Tung Yu <sam@bristolgram.uk>
2026-02-17 19:59:12 +0000

    add container

--- lgdxrobot_cloud_adapter/include/lgdxrobot_cloud_adapter/CloudAdapter.hpp
+++ lgdxrobot_cloud_adapter/include/lgdxrobot_cloud_adapter/CloudAdapter.hpp
@@ -4,15 +4,21 @@
 #include "rclcpp/rclcpp.hpp"
 #include "RobotStatus.hpp"
 
+namespace LGDXRobotCloud
+{
+  
 class CloudAdapter : public rclcpp::Node
 {
   private:
+    rclcpp::TimerBase::SharedPtr timer;
     RobotStatus::StateMachine robotStatus = RobotStatus::Offline();
 
   public:
-    CloudAdapter();
+    CloudAdapter(const rclcpp::NodeOptions &options);
 
     void Initalise();
 };
+
+}
 
 #endif // CLOUD_ADAPTER_HPP
--- lgdxrobot_cloud_adapter/src/CloudAdapter.cpp
+++ lgdxrobot_cloud_adapter/src/CloudAdapter.cpp
@@ -1,11 +1,22 @@
+#include <chrono>
+#include <rclcpp_components/register_node_macro.hpp>
 #include "lgdxrobot_cloud_adapter/CloudAdapter.hpp"
 
-CloudAdapter::CloudAdapter() : Node("lgdxrobot_cloud_adapter")
-{}
+namespace LGDXRobotCloud
+{
+  
+CloudAdapter::CloudAdapter(const rclcpp::NodeOptions &options) : Node("lgdxrobot_cloud_adapter_node", options)
+{
+  timer = this->create_wall_timer(std::chrono::microseconds(1), [this]() {this->Initalise();});
+}
 
 void CloudAdapter::Initalise()
 {
-  RCLCPP_INFO(this->get_logger(), "Initialising Cloud Adapter");
+  timer->cancel();
 
+  RCLCPP_INFO(this->get_logger(), "Initialising Cloud Adapter");
+}
 
 }
+
+RCLCPP_COMPONENTS_REGISTER_NODE(LGDXRobotCloud::CloudAdapter)
